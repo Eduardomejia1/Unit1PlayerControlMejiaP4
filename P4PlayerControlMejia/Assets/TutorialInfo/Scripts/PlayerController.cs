aad319a03dcf60f9afbf3ab387449a6e4fc6fde4
Eduardomejia1 <730845@student.dusd.net>
2026-02-06 11:45:41 -0800

    Edited scripts
    Tested and edited scripts.

--- P4PlayerControlMejia/Assets/TutorialInfo/Scripts/PlayerController.cs
+++ P4PlayerControlMejia/Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -1,7 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +16,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
