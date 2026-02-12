d7d8cdc9a56a7c06dae86a90b6d051e189b2e979
Eduardomejia1 <730845@student.dusd.net>
2026-02-12 11:47:24 -0800

    Move the car
    Made the car move.

--- P4PlayerControlMejia/Assets/TutorialInfo/Scripts/PlayerController.cs
+++ P4PlayerControlMejia/Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
+    // Private Variables
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,8 +19,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
         // Move the vehicle forward 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
