2441b4dceb2072b5e16443b7f07754aaf205e2cd
Eduardomejia1 <730845@student.dusd.net>
2026-05-12 11:30:33 -0700

    Added comments to my script
    Added comments to script in my project.

--- P4PlayerControlMejia/Assets/TutorialInfo/Scripts/PlayerController.cs
+++ P4PlayerControlMejia/Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -19,11 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         
-        // Move the vehicle forward 
+        // We move the vehicle forward 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
